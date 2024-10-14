fded91caeec471124f79faccecb36cff5c808e03
OXXDDWW <109534967+wukonggd@users.noreply.github.com>
2024-10-14 14:54:26 +0800

    Update mp3.js

--- assets/js/mp3.js
+++ assets/js/mp3.js
@@ -1,12 +1,12 @@
-let isAudioPlayed = false;
+        let isAudioPlaying = false;
 
-document.body.addEventListener('click', function() {
-   if (!isAudioPlayed) {
-    var audio = document.getElementById('audio');
-    audio.play();
-    isAudioPlayed = true;
-}
-});
+        document.body.addEventListener('click', function() {
+            var audio = document.getElementById('audio');
+            if (!isAudioPlaying) {
+                audio.play();
+                isAudioPlaying = true;
+            }
+        });
 
         document.getElementById('toggleButton').addEventListener('click', function() {
             var audio = document.getElementById('audio');
