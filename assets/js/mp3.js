eda45b26205b81b5dccde279037d24ad14cf179a
OXXDDWW <109534967+wukonggd@users.noreply.github.com>
2024-10-14 15:07:55 +0800

    Update mp3.js

--- assets/js/mp3.js
+++ assets/js/mp3.js
@@ -14,3 +14,16 @@
             audio.currentTime = 0; // Reset audio to start
             isAudioPlayed = false;
         });
+
+        document.getElementById('controlButton').addEventListener('click', function() {
+            var audio = document.getElementById('audio');
+            if (!isAudioPlaying) {
+                audio.play();
+                this.textContent = 'Stop Music';
+            } else {
+                audio.pause();
+                audio.currentTime = 0; // Reset audio to start
+                this.textContent = 'Play Music';
+            }
+            isAudioPlaying = !isAudioPlaying;
+        });
